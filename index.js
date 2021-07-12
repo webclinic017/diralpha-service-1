14dd97a6225a2039bda29b3890059be6ff36d6aa
rahul <haxdds@gmail.com>
2021-07-11 23:12:33 -0400

    added linter, husky and prehook

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 // routes
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'Hello world!!!'
-    });
+	res.status(200).json({
+		message: 'Hello world!!!'
+	});
 });
-
+console.log('jadjahd ajsdhksadasdasdasdasdasdasdasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
 module.exports = app;
