aec3ca4797390a60c93f661da1dcb66d75974e32
rahul <haxdds@gmail.com>
2021-07-14 22:50:54 -0400

    forgot to add this guy

--- alpaca-gateway/broker/accounts/account-creation-gateway.js
+++ alpaca-gateway/broker/accounts/account-creation-gateway.js
@@ -1,20 +1,17 @@
 const axios = require('axios');
 
-const alpacaAccountEndpoint = `${process.env.ALPACA_ACCOUNT_ENDPOINT}`;
-
 const accountCreationGateway = {
 
   async createAccount(res, alpacaAccountObject) {
     // TODO
-    axios.post(alpacaAccountEndpoint,
+    axios.post(process.env.ALPACA_ACCOUNT_ENDPOINT,
       alpacaAccountObject)
       .then((response) => {
         // console.log(response.data);
         // console.log(response.status);
         // console.log(response.statusText);
         // console.log(response.headers);
-        console.log('hey');
-        // res.send('hi');
+        console.log(response.headers);
         return response;
       }, (error) => {
         console.log(error);
