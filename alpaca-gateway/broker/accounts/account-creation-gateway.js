fe677f97005a6c6078544197f34174e8e6424bb9
rahul <haxdds@gmail.com>
2021-07-14 22:00:57 -0400

    updated alpaca account-creation gateway

--- alpaca-gateway/broker/accounts/account-creation-gateway.js
+++ alpaca-gateway/broker/accounts/account-creation-gateway.js
@@ -1,21 +1,20 @@
 const axios = require('axios');
 
-const alpacaAccountEndpoint = process.env.ALPACA_ACCOUNT_ENDPOINT;
+const alpacaAccountEndpoint = 'https://CKQ76245TJS5U0EPO1TT:dM1IHTyDnp7wi6UzooIXl28DAAZ4fA6fLfQfMgrH@broker-api.sandbox.alpaca.markets/v1/accounts';
 
-const accountCreationGateWay = {
+const accountCreationGateway = {
 
-  async createAccount(alpacaAccountObject) {
+  async createAccount(res, alpacaAccountObject) {
     // TODO
-    axios.post({
-      alpacaAccountEndpoint,
-      alpacaAccountObject,
-    })
+    axios.post(alpacaAccountEndpoint,
+      alpacaAccountObject)
       .then((response) => {
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
+        // console.log(response.data);
+        // console.log(response.status);
+        // console.log(response.statusText);
+        // console.log(response.headers);
+        console.log('hey');
+        // res.send('hi');
         return response;
       }, (error) => {
         console.log(error);
@@ -23,4 +22,4 @@ const accountCreationGateWay = {
   },
 };
 
-module.exports = accountCreationGateWay;
+module.exports = accountCreationGateway;
--- controllers/accounts/accounts-controller.js
+++ controllers/accounts/accounts-controller.js
@@ -1,10 +1,14 @@
+const accountCreationGateway = require('../../alpaca-gateway/broker/accounts/account-creation-gateway');
+
 const accountsController = {
 
   register(req, res) {
     /* TODO:
         Need to check if user already has account registered. Users cannot multiple accounts
     */
-    res.send(req.body);
+    const response = accountCreationGateway.createAccount(res, req.body);
+
+    res.send(`hi ${response}`);
   },
 
 };
