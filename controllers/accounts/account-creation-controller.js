6c8978003eda72d21196df40b8cf06e3d03e04b7
rahul <haxdds@gmail.com>
2021-07-16 09:19:25 -0400

    added alpaca response handlers on account-creation-controller

--- controllers/accounts/account-creation-controller.js
+++ controllers/accounts/account-creation-controller.js
@@ -1,26 +1,36 @@
 const accountCreationGateway = require('../../alpaca-gateway/broker/accounts/account-creation-gateway');
 
-const accountsController = {
+const accountCreationController = {
 
+  /**
+   * Entry point for handling an incoming request from the account creation endpoint
+   * @param {Express Request} req the incoming HTTP request
+   * @param {Express Response} res the HTTP response object used to service the request
+   */
   register(req, res) {
     /* TODO:
         Need to check if user already has account registered. Users cannot multiple accounts
     */
     const response = accountCreationGateway.createAccount(res, req.body);
 
-    console.log(`HI! ${response}`);
     response.then(
       (value) => {
-        console.log(`VALUE: ${value}`);
-        res.send('Hello');
+        this.handleAlpacaResponse(req, res, value);
       },
       (error) => {
-        console.log(`ERROR: ${error}`);
-        res.send(error);
+        this.handleAlpacaError(req, res, error);
       },
     );
   },
 
+  // handleValidAlpacaResponse(routerReq, routerRes, alpacaRes) {
+
+  // },
+
+  // handleAlpacaError(routerReq, routerRes, alpacaRes) {
+
+  // },
+
 };
 
-module.exports = accountsController;
+module.exports = accountCreationController;
