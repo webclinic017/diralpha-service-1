d4eabc532d3a9b862df87d589af28e4663affcbc
= <=>
2021-07-23 11:17:43 -0400

    added migration and seeds for user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+const { Model } = require('sequelize');
+
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+    async validPassword(password) {
+      // asynchronously check if input password matches password on DB
+      const match = await bcrypt.compare(password, this.password);
+      return match;
+    }
+  }
+  User.init({
+    emailAddress: DataTypes.STRING,
+    password: DataTypes.STRING,
+    lastLogin: DataTypes.DATE,
+  },
+  {
+    hooks: {
+      beforeCreate: async (user, options) => {
+        // hash password with salt of 2^10 iterations
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+      beforeUpdate: async (user, options) => {
+        // hash password with salt of 2^10 iterations
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+    },
+    sequelize,
+    modelName: 'User',
+    underscored: true,
+  });
+
+  return User;
+};
---
+++ seeders/20210721204621-test-user.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+/* eslint-disable lines-around-directive */
+/* eslint-disable no-unused-vars */
+const bcrypt = require('bcryptjs');
+const userData = require('../config/MOCK_USER_DATA.json');
+
+const mockUserDataWithLogins = userData.map((user) => ({
+  email_address: user.email_address,
+  password: bcrypt.hashSync(user.password, 10),
+  last_login: new Date(),
+  created_at: new Date(),
+  updated_at: new Date(),
+}));
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', mockUserDataWithLogins, {}),
+  down: (queryInterface, Sequelize) => {
+    const { Op } = Sequelize;
+    const emails = mockUserDataWithLogins.map(({ email_address }) => email_address);
+
+    return queryInterface.bulkDelete('Users', { email_address: { [Op.in]: emails } }, {}).catch((error) => console.log(error));
+  },
+};
