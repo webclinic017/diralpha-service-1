f99f002f3a04527657c1c679ff9a8ed23d40e2b1
haxdds <haxdds@gmail.com>
2021-07-29 18:40:41 -0400

    small syntax improvements to user seed file

--- seeders/20210721204621-test-user.js
+++ seeders/20210721204621-test-user.js
@@ -4,12 +4,13 @@
 const bcrypt = require('bcryptjs');
 const userData = require('../config/MOCK_USER_DATA.json');
 
+const currentTime = new Date();
 const mockUserDataWithLogins = userData.map((user) => ({
   email_address: user.email_address,
   password: bcrypt.hashSync(user.password, 10),
-  last_login: new Date(),
-  created_at: new Date(),
-  updated_at: new Date(),
+  last_login: currentTime,
+  created_at: currentTime,
+  updated_at: currentTime,
 }));
 
 module.exports = {
@@ -18,6 +19,6 @@ module.exports = {
     const { Op } = Sequelize;
     const emails = mockUserDataWithLogins.map(({ email_address }) => email_address);
 
-    return queryInterface.bulkDelete('users', { email_address: { [Op.in]: emails } }, {}).catch((error) => console.log(error));
+    return queryInterface.bulkDelete('users', { email_address: { [Op.in]: emails } }, {});
   },
 };
