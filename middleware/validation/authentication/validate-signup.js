f5430bf8eaa8290e7014a6d2c4cd173d4928f1eb
haxdds <haxdds@gmail.com>
2021-08-03 13:28:18 -0400

    added schemas directiory for validation schemas

---
+++ middleware/validation/authentication/validate-signup.js
@@ -0,0 +1,9 @@
+// const joi = require('joi');
+
+// const validate = async (req, res, next) => {
+// console.log('Validate: ', req.body);
+//   const { value, error } = userSchema.validate(req.body);
+// next();
+// };
+
+// module.exports = validate;
--- routes/accounts-api/accounts/create-brokerage-account.js
+++ routes/accounts-api/accounts/create-brokerage-account.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const accountValidationMiddleware = require('../../../middleware/validation/accounts/account-validation');
+const accountValidation = require('../../../middleware/validation/accounts/validate-account');
 const accountsController = require('../../../controllers/accounts/accounts-controller');
 
 const expressJwtAuth = require('../../../middleware/authentication/jwt-auth');
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.use('/register', expressJwtAuth);
 router.use('/register', attachCurrentUser);
-router.use('/register', accountValidationMiddleware.validate);
+router.use('/register', accountValidation.validate);
 router.post('/register', accountsController.register);
 
 module.exports = router;
